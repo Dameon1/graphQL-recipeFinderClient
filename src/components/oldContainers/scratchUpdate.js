7954e2eedb1c666c5b9232beb9529b23f249694f
Dameon Mendoza <dameonmendoza@gmail.com>
2019-01-24 11:46:35 -0700

    removed "old" files

--- src/components/oldContainers/scratchUpdate.js
+++
@@ -1,9 +0,0 @@
-import gql from 'graphql-tag';
-
-export default gql`
-  mutation updateNumber($index: String!,$value: String!) {
-    updateNumber(value:$value,index:$index) @client {
-      number
-    }
-  }
-`;
