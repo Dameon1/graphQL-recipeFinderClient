7b0ff8c079dc08161608cbab8dcbe503f555a973
Dameon Mendoza <dameonmendoza@gmail.com>
2019-02-10 11:40:18 -0700

    organized styles/index

--- src/components/styles/index.js
+++ src/components/styles/index.js
@@ -1,46 +1,52 @@
-//TODO: ******LABEL AND SEPERATE*************
+//Header
+export { default as Headline } from "./headline";
+export { default as HeadlineText } from "./headlineText";
+export { default as NavButton } from "./navButton";
 
+//LANDING PAGE
+export { default as Landing } from "./landing";
+export { default as LandingHeader } from "./landingHeader";
+export { default as LandingText } from "./landingText";
+export { default as LandingSmallPrint } from "./landingHeader";
+export { default as LandingButton } from "./landingButton";
+
+//SIGN IN AND CREATE
+export { default as Form } from "./form";
+export { default as Label } from "./label";
+export { default as UserSignIn } from "./userSignIn";
+export { default as SignInInput } from "./signInInput";
+export { default as SignInButton } from "./signInButton";
+export { default as ErrorStyles } from "./errorStyles";
+
+//ADD AND SEARCH RECIPES PAGE
+export { default as SearchForm } from "./searchForm";
+export { default as DashboardHeading } from "./dashboardHeading";
+export { default as RecipeSearchButton } from "./recipeSearchButton";
 export { default as AddIngredientButton } from "./addIngredientButton";
 export { default as AddIngredientForm } from "./addIngredientForm";
 export { default as AddIngredientLabel } from "./addIngredientLabel";
 export { default as AddIngredientInput } from "./addIngredientInput";
-
-export { default as DashboardHeading } from "./dashboardHeading";
-export { default as ErrorStyles } from "./errorStyles";
-export { default as Form } from "./form";
-export { default as Headline } from "./headline";
-export { default as HeadlineText } from "./headlineText";
-
 export { default as IngredientBox } from "./ingredientBox";
 
-export { default as Label } from "./label";
-export { default as Landing } from "./landing";
-export { default as LandingButton } from "./landingButton";
-export { default as LandingHeader } from "./landingHeader";
-export { default as LandingSmallPrint } from "./landingHeader";
-export { default as LandingText } from "./landingText";
-
+//MULTIPLE RECIPE DISPLAYS GUEST AND USERS SAVED
+export { default as UserNoSavedRecipes } from "./userNoSavedRecipes";
+export { default as NoSavedRecipeText } from "./noSavedRecipeText";
+export { default as RecipeDisplayContainer } from "./recipeDisplayContainer";
 export { default as MultiRecipeButton } from "./multiRecipeButton";
 export { default as MultiRecipeImage } from "./multiRecipeImage";
+export { default as SmallRecipeContainer } from "./smallRecipeContainer";
+export { default as SmallRecipeTitleText } from "./smallRecipeTitle";
 
-export { default as NavButton } from "./navButton";
-export { default as RecipeDisplayContainer } from "./recipeDisplayContainer";
-export { default as RecipeSearchButton } from "./recipeSearchButton";
-export { default as SearchForm } from "./searchForm";
-export { default as SignInButton } from "./signInButton";
-export { default as SignInInput } from "./signInInput";
+//SINGLE RECIPE DISPLAY
 export { default as SingleRecipeContainer } from "./singleRecipeContainer";
 export { default as SingleRecipeImage } from "./singleRecipeImage";
 export { default as SingleRecipeInstructionText } from "./singleRecipeInstructionText";
 export { default as SingleRecipeTitleText } from "./singleRecipeTitleText";
-export { default as UserSignIn } from "./userSignIn";
-export { default as SmallRecipeContainer } from "./smallRecipeContainer";
-export { default as SmallRecipeTitleText } from "./smallRecipeTitle";
 export { default as RecipeInstructionText } from "./recipeInstructionText";
 export { default as SingleRecipeActionButton } from "./singleRecipeActionButton";
-export { default as FooterContainer } from "./footerContainer";
+export { default as RecipeLinkButton } from "./recipeLinkButton";
+
+// Footer
 export { default as FooterText } from "./footerText";
 export { default as FooterLink } from "./footerLink";
-export { default as UserNoSavedRecipes } from "./userNoSavedRecipes";
-export { default as NoSavedRecipeText } from "./noSavedRecipeText";
-export { default as RecipeLinkButton } from "./recipeLinkButton";
+export { default as FooterContainer } from "./footerContainer";
