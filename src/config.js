118fcd1bec212181e055dc71f9444c290bf1cafa
Dameon Mendoza <dameonmendoza@gmail.com>
2024-01-02 07:04:57 -0700

    update server url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:4000"
-    : "https://what2eat-graphql.herokuapp.com/";
+    : "https://what2eat.onrender.com";
 
 module.exports = API_BASE_URL;
